44d8294431b987b2a1ea7688190452440fbc7c61
Sandra Schuhmacher <coding@voiding-warranties.de>
2017-07-14 13:49:07 -0700

    finally I got the math right. ready for rviz testing

--- kuka_arm/scripts/Inverse_Kinematics.py
+++ kuka_arm/scripts/Inverse_Kinematics.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class IK:
 
-    def __init__(self,T0_1, T1_2, T2_3, symbols, q1, q2, q3):
+    def __init__(self,T0_1, T1_2, T2_3, symbols, q1, q2, q3, R_corr):
         self.end_effector_length = 0.303 #0.453 #0.303
         self.Rrpy = None
         self.a_1 = 0.35
@@ -21,14 +21,30 @@ class IK:
 	self.q2 = q2
 	self.q3 = q3
         self.symbols = symbols
+	self.R_corr = R_corr
 
     # Build a rotation matrix from the Roll, Pitch and Yaw angles
     def get_wrist_rot_matrix(self, roll, pitch, yaw):
+
+        R_z =  Matrix([[     cos(yaw), -sin(yaw),             0],
+                       [     sin(yaw),  cos(yaw),             0],
+                       [              0,           0,             1]])
+
+        R_y =  Matrix([[  cos(pitch),           0, sin(pitch)],
+                       [              0,           1,             0],
+                       [ -sin(pitch),           0, cos(pitch)]])
+    
+        R_x = Matrix([[ 1,              0,        0],
+                     [ 0,        cos(roll), -sin(roll)],
+                     [ 0,        sin(roll),  cos(roll)]])
+    
+        Rrpy = simplify ( R_z * R_y * R_x )
             
-        # Rrpy = Rx * Ry * Rz
-        Rrpy = Matrix([[    cos(yaw)*cos(pitch),   cos(yaw)*sin(pitch)*sin(roll) - sin(yaw)*cos(roll),    cos(yaw)*sin(pitch)*cos(roll) + sin(yaw)*sin(roll)],
-                       [    sin(yaw)*cos(pitch),   sin(yaw)*sin(pitch)*sin(roll) + cos(yaw)*cos(roll),    sin(yaw)*sin(pitch)*cos(roll) - cos(yaw)*sin(roll)],
-                       [            -sin(pitch),             cos(pitch)*sin(roll),                                       cos(pitch)*cos(roll)               ]])
+     
+        # Rrpy = Rz * Ry * Rx
+        Rrpy_2 = Matrix([[    cos(yaw)*cos(pitch),   cos(yaw)*sin(pitch)*sin(roll) - sin(yaw)*cos(roll),    cos(yaw)*sin(pitch)*cos(roll) + sin(yaw)*sin(roll)],
+                         [    sin(yaw)*cos(pitch),   sin(yaw)*sin(pitch)*sin(roll) + cos(yaw)*cos(roll),    sin(yaw)*sin(pitch)*cos(roll) - cos(yaw)*sin(roll)],
+                         [            -sin(pitch),             cos(pitch)*sin(roll),                                       cos(pitch)*cos(roll)               ]])
 
         self.Rrpy = Rrpy
         return 
@@ -59,34 +75,38 @@ class IK:
         
         theta1 = atan2(wy, wx)
         
-        x_c = sqrt( wx**2 + wy**2 ) - self.a_1 # Subtract a1 as horizontal offset from robot-base
+	r = sqrt( wx**2 + wy**2) - self.a_1
+        #x_c = wx - self.a_1 # Subtract a1 as horizontal offset from robot-base
         z_c = wz - self.d_1 # Subtract d1 as vertical offset from robot base
-
-        L_25 = sqrt(x_c**2 + z_c**2)
-        L_35 = sqrt(self.a_3**2 + self.d_4**2)
-
-        cos_theta_3 = ( L_25**2 - self.a_2**2 - L_35**2) / ( 2 * self.a_2 * L_35 )
-        #theta3 = atan2( cos_theta_3, sqrt( 1 - cos_theta_3**2 ))
-        q3 = atan2( sqrt( 1 - cos_theta_3**2 ), cos_theta_3 )
-        theta3 = ( q3 - np.pi/2 ).evalf()
-        theta3_alt = ( -q3 + np.pi/2 ).evalf()
         
-        # theta2 = atan2( z_c, x_c ) - atan2(a_2*sin(theta3), a_2+L_35*cos(theta3))
-        # theta2b = atan2( z_c, x_c ) - atan2(a_2*sin(theta3b), a_2+L_35*cos(theta3b))
-        beta_2 = atan2( z_c, x_c )
-        cos_beta_1 = ( ( -L_35**2 + L_25**2 + self.a_2**2 ) / ( 2 * L_25 * self.a_2 ) )
-        beta_1 = atan2(  cos_beta_1, sqrt( 1 - cos_beta_1**2 ) ) 
+        #L_25 = sqrt(x_c**2 + z_c**2)
+	L_25 = sqrt(r**2 + z_c**2)
+        L_35 = sqrt(self.a_3**2+ self.d_4**2)
+        
+        beta_2 = atan2( z_c, r )
+        #beta_2 = atan2( z_c, x_c )
+        cos_beta_1 = ( ( L_35**2 - L_25**2 - self.a_2**2 ) / ( - 2 * L_25 * self.a_2 ) )
+        #beta_1 = acos ( cos_beta_1 )
+        beta_1 = atan2(  sqrt( 1 - cos_beta_1**2 ), cos_beta_1 ) 
+        #beta_12 = atan2(  - sqrt( 1 - cos_beta_1**2 ), cos_beta_1 ) 
         theta2 = np.pi/2 - beta_1 - beta_2
+	# flipped 
+        
+        beta_4 = atan2( - self.a_3, self.d_4)
+        cos_beta_3 = ( L_25**2 - self.a_2**2 - L_35**2) / ( -2 * self.a_2 * L_35 )
+        #beta_3 = acos ( cos_beta_3 )
+        beta_3 = atan2( sqrt( 1 - cos_beta_3**2 ), cos_beta_3 )
+        #beta_32 = atan2( - sqrt( 1 - cos_theta_3**2 ), cos_theta_3 )
+        theta3 = np.pi/2 - beta_3 - beta_4
 
-        print("theta1,2,3:",theta1, theta2, theta3)
-            
+
         return theta1.evalf(), theta2.evalf(), theta3.evalf()
 
     def transform_to_wc(self, theta1, theta2, theta3):
      
         R0_3 = simplify( self.T0_1 * self.T1_2 * self.T2_3 )
         R0_3 = R0_3.evalf(subs={self.q1: theta1, self.q2: theta2, self.q3: theta3})
-        R3_6_eval = R0_3.inv() * self.Rrpy
+        R3_6_eval = R0_3.inv() * self.Rrpy * self.R_corr
         return R3_6_eval        
 
     def get_theta_456(self, R3_6_eval):
@@ -101,10 +121,10 @@ class IK:
         theta5 = atan2( sin_q5, r23 ).evalf()
         
         if( sin_q5 < 0 ):
-            theta4 = atan2(-r33,  r13).evalf()
-            theta6 = atan2( r22, -r21).evalf()
+            theta4 = atan2( -r33,  r13).evalf()
+            theta6 = atan2(  r22, -r21).evalf()
         else:
-            theta4 = atan2( r33,  -r13).evalf()
-            theta6 = atan2( -r22, r21).evalf()
+            theta4 = atan2(  r33, -r13).evalf()
+            theta6 = atan2( -r22,  r21).evalf()
         
         return theta4, theta5, theta6
